9f454f59387def78f85bce7102db78d61fecca78
SteveLEEEEE <yuheng.li@monash.edu>
2025-01-19 18:50:14 +1100

    include subtask

--- h/app.py
+++ h/app.py
@@ -97,6 +97,7 @@ def includeme(config):  # pragma: no cover
     config.include("h.links")
     config.include("h.notification")
 
+    config.include("h.subtask")
     _configure_sentry(config)
 
     # pyramid-sanity should be activated as late as possible
