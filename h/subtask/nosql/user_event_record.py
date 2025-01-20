6ee3226b060bfffad081005c7fc1f571d3df105e
SteveLEEEEE <yuheng.li@monash.edu>
2025-01-20 23:06:59 +1100

    test fix

--- h/subtask/nosql/user_event_record.py
+++ h/subtask/nosql/user_event_record.py
@@ -16,13 +16,13 @@ class UserEventRecord(JsonModel):
     session_id: str = Field(full_text_search=True, sortable=True)
     task_name: Optional[str] = Field(full_text_search=True, sortable=True)
     description: str = Field(full_text_search=True, sortable=True)
-    target_uri: str = Field(full_text_search=True, sortable=True)
-    start: int = Field(index=True)
+    target_uri: Optional[str]
+    start: Optional[int]
+    backdate: Optional[int]
     completed: int = Field(index=True)
     userid: str = Field(index=True)
     groupid: str = Field(index=True)
     shared: int = Field(index=True)
-    # steps: List[UserEvent] = Field(sortable=True)
 
 
 def fetch_all_user_event_record():
