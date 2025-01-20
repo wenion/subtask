6ac6ebb56f2a70329980394e7b3f881fd8201fbb
SteveLEEEEE <yuheng.li@monash.edu>
2025-01-20 23:50:11 +1100

    bug test

--- h/subtask/task.py
+++ h/subtask/task.py
@@ -61,6 +61,7 @@ def load_all_process_models():
         for pm in process_models:
             print(pm.session_id, pm.creator)
             print(fetch_user_event_record_by_session(session_id=pm.session_id))
+            print(fetch_user_event_record_by_user(userid=pm.creator))
             record = fetch_user_event_record_by_session_id(session_id=pm.session_id, userid=pm.creator)
             print(record)
             if not record:
