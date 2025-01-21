e18f1f79054bd804dbf310fc68b2867bc943a6f3
SteveLEEEEE <yuheng.li@monash.edu>
2025-01-21 23:12:34 +1100

    fix

--- h/subtask/task.py
+++ h/subtask/task.py
@@ -562,7 +562,7 @@ def process_messages(settings, subscribe_routing_key, produce_routing_key):
         """
 
         # TODO: change implementation to use windowId and TabId, ClientId will not work properly
-        print("payload", payload["type"], payload["custom"], payload["label"])
+        print("payload", payload)
         global user_status
         current_time = datetime.now().timestamp() * 1000
         message = ""
