8afc4e5a2efcb585193f4db74ea0c36355134eea
SteveLEEEEE <yuheng.li@monash.edu>
2025-02-27 09:49:11 +1100

    fix

--- h/subtask/nosql/user_event_record.py
+++ h/subtask/nosql/user_event_record.py
@@ -41,11 +41,13 @@ def fetch_user_event_record_by_session_id(session_id, userid):
 
 
 def fetch_user_event_record_by_pk(pk):
-    query = UserEventRecord.find(UserEventRecord.pk == pk)
-    if query:
+    try:
+        query = UserEventRecord.find(UserEventRecord.pk == pk)
         total = query.all()
         return total[0] if len(total) > 0 else None
-    return None
+    except:
+        return None
+
 
 
 def fetch_user_event_record_by_session(session_id):
