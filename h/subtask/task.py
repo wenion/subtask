00a0616d5395c9f22f12678b2ea4ec5cd2d3e566
Yuheng Li <yuheng.li@monash.edu>
2025-06-02 00:24:21 +0800

    print

--- h/subtask/task.py
+++ h/subtask/task.py
@@ -198,6 +198,7 @@ def expert_steps(new_trace, new_pm, threshold=0.7):
         G.add_edge(act_from, act_to, weight=freq)
     b_centrality = nx.betweenness_centrality(G, normalized=True, endpoints=False)
     b_centrality = dict(sorted(b_centrality.items(), key=lambda x: x[1], reverse=True))
+    print(b_centrality)
     new_pm_places = [val.name for val in new_pm[0].places]
     key_steps = [k for k, v in b_centrality.items() if k in new_pm_places and v > 0.1]
     for pm in conforming_pm:
